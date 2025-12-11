68a90783bdb822d80d4e0c906627f98da5f6e25c
gonzoblasco <gonzoblasco@gmail.com>
2025-12-11 19:57:12 -0300

    refactor: use col-span-full for loading state wrapper

--- coolmovies-frontend/src/features/reviews/templates/Reviews.tsx
+++ coolmovies-frontend/src/features/reviews/templates/Reviews.tsx
@@ -39,7 +39,7 @@ const Reviews = () => {
             <div
               role="status"
               aria-live="polite"
-              className="col-span-1 md:col-span-2 lg:col-span-3 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"
+              className="col-span-full grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"
             >
               <span className="sr-only">Loading movies...</span>
               {Array.from(new Array(6)).map((_, index) => (
